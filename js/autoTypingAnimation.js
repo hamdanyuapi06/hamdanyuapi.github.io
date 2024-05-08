b90552ecc079082ce3f3f938da2b322e789f7e48
hamdanyuapi06 <hamdanyuapi536@gmail.com>
2024-05-08 21:02:02 +0700

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, Nama saya Hamdan Yuapi</span> saya adalah seorang pengembang sofware developer merancang sebuah aplikasi Lulusan Universitas Pelita Bangsa jurusan Informatika. Memiliki kemampuan yang sangat baik dalam menggunakan bahasa pemrograman PHP untuk menganalisis data dan meningkatkan efisiensi operasional. Sedang mencari pekerjaan untuk menjadi seorang junior programmer.  "
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
